b04b25b4859865627c95920b1b47f340bf1e3242
hotenglish <hotenglish@outlook.com>
2018-07-22 10:52:56 +0800

    reset springboot

--- springboot/ch03/target/classes/db/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO READER (USERNAME, PASSWORD, FULLNAME) VALUES ('walt', 'marceline', 'Walt Disney');
-INSERT INTO BOOK (USERNAME, ISBN, TITLE, AUTHOR, DESCRIPTION) VALUES ('walt','GB210004','spring_in_action','walt','technology');
