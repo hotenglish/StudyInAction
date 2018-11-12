f1869b7717f14fc2f7cc9bb33907f2b7889143c4
hotenglish <hotenglish@outlook.com>
2018-11-12 14:42:04 +0800

    update chap13

---
+++ SpringLearning4/chapter13/Caching/src/main/java/spittr/domain/Spittle.java
@@ -0,0 +1,34 @@
+package spittr.domain;
+
+import java.util.Date;
+
+public class Spittle {
+
+    private final Long id;
+    private final Spitter spitter;
+    private final String message;
+    private final Date postedTime;
+
+    public Spittle(Long id, Spitter spitter, String message, Date postedTime) {
+        this.id = id;
+        this.spitter = spitter;
+        this.message = message;
+        this.postedTime = postedTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Spitter getSpitter() {
+        return spitter;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getPostedTime() {
+        return postedTime;
+    }
+}
