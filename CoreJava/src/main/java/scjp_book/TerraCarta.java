bef1d423b35b7462dc7114201660c622811403b5
hotenglish <hotenglish@outlook.com>
2019-03-01 16:24:09 +0800

    update core java

---
+++ CoreJava/src/main/java/scjp_book/TerraCarta.java
@@ -0,0 +1,11 @@
+package scjp_book;
+
+//import static scjp_book.UseAnother;
+import static scjp_book.UseAnother.*;
+
+public class TerraCarta {
+
+    public double halfway(){
+        return DIAMETER/2.0;
+    }
+}
---
+++ CoreJava/src/main/java/scjp_book/UseAnother.java
@@ -0,0 +1,5 @@
+package scjp_book;
+
+public class UseAnother {
+    public static final double DIAMETER=12756.32;
+}
